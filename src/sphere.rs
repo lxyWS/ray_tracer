0c6013a97355b351f71145ecbf9899830b03b8ac
lxyWS <348677673@qq.com>
2025-06-29 15:19:26 +0800

    Update sphere.rs

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,14 +1,16 @@
+use crate::aabb::Aabb;
 use crate::hittable::{HitRecord, Hittable};
 use crate::interval::Interval;
 use crate::material::MaterialPtr;
 use crate::ray::Ray;
-use crate::vec3::{Point3, dot};
+use crate::vec3::{Point3, Vec3, dot};
 
 /// 表示三维空间中的球体
 pub struct Sphere {
-    center: Point3, // 球心坐标
-    radius: f64,    // 半径（确保非负）
+    center: Ray, // 球心坐标
+    radius: f64, // 半径（确保非负）
     mat: MaterialPtr,
+    bbox: Aabb, // bounding box
 }
 
 impl Sphere {
@@ -17,11 +19,25 @@ impl Sphere {
     /// # 参数
     /// - `center`: 球心坐标
     /// - `radius`: 半径（负值会被截断为0）
-    pub fn new(center: Point3, radius: f64, mat: MaterialPtr) -> Self {
+    pub fn new(static_center: Point3, radius: f64, mat: MaterialPtr) -> Self {
+        let rvec = Vec3::new(radius, radius, radius);
         Self {
-            center,
+            center: Ray::with_origin_dir(static_center, Vec3::new(0.0, 0.0, 0.0)),
             radius: radius.max(0.0), // 确保半径非负
             mat: mat,
+            bbox: Aabb::from_points(static_center - rvec, static_center + rvec),
+        }
+    }
+
+    pub fn new_moving(center1: Point3, center2: Point3, radius: f64, mat: MaterialPtr) -> Self {
+        let rvec = Vec3::new(radius, radius, radius);
+        let box1 = Aabb::from_points(center1 - rvec, center1 + rvec);
+        let box2 = Aabb::from_points(center2 - rvec, center2 + rvec);
+        Self {
+            center: Ray::with_origin_dir(center1, center2 - center1),
+            radius: radius.max(0.0),
+            mat: mat,
+            bbox: Aabb::from_aabbs(box1, box2),
         }
     }
 }
@@ -29,7 +45,8 @@ impl Sphere {
 /// 实现Hittable trait，使球体可被射线击中
 impl Hittable for Sphere {
     fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
-        let oc = self.center - *r.origin();
+        let current_center = self.center.at(r.time());
+        let oc = current_center - *r.origin();
         let a = r.direction().length_squared();
         let h = dot(r.direction(), &oc);
         let c = oc.length_squared() - self.radius * self.radius;
@@ -58,7 +75,7 @@ impl Hittable for Sphere {
         // 记录交点信息
         rec.t = root;
         rec.p = r.at(rec.t);
-        let outward_normal = (rec.p - self.center) / self.radius;
+        let outward_normal = (rec.p - current_center) / self.radius;
         rec.set_face_normal(r, outward_normal);
         rec.mat = Some(self.mat.clone());
 
@@ -68,4 +85,10 @@ impl Hittable for Sphere {
 
         true
     }
+
+    fn bounding_box(&self) -> Aabb {
+        self.bbox
+    }
 }
+
+unsafe impl Sync for Sphere {}
