fec330197d0ee7450cba91d6a00a1e571391e700
lxyWS <348677673@qq.com>
2025-07-02 11:19:58 +0800

    Add files via upload

---
+++ src/texture.rs
@@ -0,0 +1,134 @@
+use core::fmt;
+use std::sync::Arc;
+
+use crate::{color::Color, perlin::Perlin, rtw_stb_image::RtwImage, vec3::Point3};
+
+pub trait Texture: Sync + Send + fmt::Debug {
+    fn value(&self, u: f64, v: f64, p: &Point3) -> Color;
+}
+
+#[derive(Debug, Clone)]
+pub struct SolidColor {
+    albedo: Color,
+}
+
+impl SolidColor {
+    pub fn new(albedo: Color) -> Self {
+        Self { albedo }
+    }
+
+    pub fn from_rgb(red: f64, green: f64, blue: f64) -> Self {
+        Self {
+            albedo: Color::new(red, green, blue),
+        }
+    }
+}
+
+impl Texture for SolidColor {
+    fn value(&self, _u: f64, _v: f64, _p: &Point3) -> Color {
+        self.albedo
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CheckerTexture {
+    inv_scale: f64,         // 缩放因子的倒数
+    even: Arc<dyn Texture>, // 偶数格子的纹理
+    odd: Arc<dyn Texture>,  // 奇数格子的纹理
+}
+
+impl CheckerTexture {
+    pub fn new(scale: f64, even: Arc<dyn Texture>, odd: Arc<dyn Texture>) -> Self {
+        Self {
+            inv_scale: 1.0 / scale,
+            even,
+            odd,
+        }
+    }
+
+    pub fn from_colors(scale: f64, c1: Color, c2: Color) -> Self {
+        Self {
+            inv_scale: 1.0 / scale,
+            even: Arc::new(SolidColor::new(c1)),
+            odd: Arc::new(SolidColor::new(c2)),
+        }
+    }
+}
+
+impl Texture for CheckerTexture {
+    fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
+        let x_integer = (self.inv_scale * p.x()).floor() as i32;
+        let y_integer = (self.inv_scale * p.y()).floor() as i32;
+        let z_integer = (self.inv_scale * p.z()).floor() as i32;
+
+        let is_even = (x_integer + y_integer + z_integer) % 2 == 0;
+
+        if is_even {
+            self.even.value(u, v, p)
+        } else {
+            self.odd.value(u, v, p)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ImageTexture {
+    rtw_image: RtwImage,
+}
+
+impl ImageTexture {
+    pub fn new(filename: &str) -> Self {
+        Self {
+            rtw_image: RtwImage::new(filename),
+        }
+    }
+}
+
+impl Texture for ImageTexture {
+    fn value(&self, u: f64, v: f64, _p: &Point3) -> Color {
+        if self.rtw_image.height() == 0 {
+            return Color::new(0.0, 1.0, 1.0);
+        }
+
+        let u = u.clamp(0.0, 1.0);
+        let v = 1.0 - v.clamp(0.0, 1.0);
+
+        let i = (u * self.rtw_image.width() as f64) as u32;
+        let j = (v * self.rtw_image.height() as f64) as u32;
+
+        let pixel = self.rtw_image.pixel_data(i, j);
+
+        let color_scale = 1.0 / 255.0;
+        Color::new(
+            color_scale * pixel[0] as f64,
+            color_scale * pixel[1] as f64,
+            color_scale * pixel[2] as f64,
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct NoiseTexture {
+    noise: Perlin,
+    scale: f64,
+}
+
+impl NoiseTexture {
+    pub fn new(scale: f64) -> Self {
+        Self {
+            noise: Perlin::new(),
+            scale,
+        }
+    }
+}
+
+impl Texture for NoiseTexture {
+    fn value(&self, _u: f64, _v: f64, p: &Point3) -> Color {
+        // let scaled_p = *p * self.scale;
+        // let noise_value = self.noise.noise(&scaled_p);
+        // // Color::new(1.0, 1.0, 1.0) * noise_value
+        // Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + noise_value)
+        let noise_value = self.noise.turb(&*p, 7);
+        Color::new(0.5, 0.5, 0.5) * (1.0 + (self.scale * p.z() + 10.0 * noise_value).sin())
+    }
+}
