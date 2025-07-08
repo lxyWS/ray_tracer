1e5555606a215a94cf0275f1938b323db0cdfc49
lxyWS <348677673@qq.com>
2025-07-08 17:03:19 +0800

    Update vec3.rs

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,241 +1,130 @@
-// vec3.rs
-use std::fmt;
-use std::ops::{
-    Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub, SubAssign,
-};
+use core::fmt;
+use std::sync::Arc;
 
-use crate::rtweekend::{random_double, random_double_range};
+use crate::{color::Color, perlin::Perlin, rtw_stb_image::RtwImage, vec3::Point3};
 
-#[derive(Copy, Clone, Debug, Default)]
-pub struct Vec3 {
-    pub e: [f64; 3],
+pub trait Texture: Sync + Send + fmt::Debug {
+    fn value(&self, u: f64, v: f64, p: &Point3) -> Color;
 }
 
-impl Vec3 {
-    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
-        Self { e: [e0, e1, e2] }
-    }
-
-    pub fn x(&self) -> f64 {
-        self.e[0]
-    }
-    pub fn y(&self) -> f64 {
-        self.e[1]
-    }
-    pub fn z(&self) -> f64 {
-        self.e[2]
-    }
-
-    pub fn length_squared(&self) -> f64 {
-        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
-    }
-
-    pub fn near_zero(&self) -> bool {
-        let s = 1e-8;
-        self.e[0].abs() < s && self.e[1].abs() < s && self.e[2].abs() < s
-    }
-
-    pub fn random() -> Self {
-        Self::new(random_double(), random_double(), random_double())
-    }
-
-    pub fn random_range(min: f64, max: f64) -> Self {
-        Self::new(
-            random_double_range(min, max),
-            random_double_range(min, max),
-            random_double_range(min, max),
-        )
-    }
-
-    pub fn length(&self) -> f64 {
-        self.length_squared().sqrt()
-    }
+#[derive(Debug, Clone)]
+pub struct SolidColor {
+    albedo: Color,
 }
 
-// 点类型别名
-pub type Point3 = Vec3;
-
-// 实现向量运算特质
-// 负号
-impl Neg for Vec3 {
-    type Output = Self;
-    fn neg(self) -> Self {
-        Self::new(-self.e[0], -self.e[1], -self.e[2])
+impl SolidColor {
+    pub fn new(albedo: Color) -> Self {
+        Self { albedo }
     }
-}
 
-// 相加
-impl Add for Vec3 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        Self::new(
-            self.e[0] + rhs.e[0],
-            self.e[1] + rhs.e[1],
-            self.e[2] + rhs.e[2],
-        )
-    }
-}
-
-// 加等于
-impl AddAssign for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        self.e[0] += rhs.e[0];
-        self.e[1] += rhs.e[1];
-        self.e[2] += rhs.e[2];
-    }
-}
-
-// 相减
-impl Sub for Vec3 {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self {
-        Self::new(
-            self.e[0] - rhs.e[0],
-            self.e[1] - rhs.e[1],
-            self.e[2] - rhs.e[2],
-        )
-    }
-}
-
-// 减等于
-impl SubAssign for Vec3 {
-    fn sub_assign(&mut self, rhs: Self) {
-        self.e[0] -= rhs.e[0];
-        self.e[1] -= rhs.e[1];
-        self.e[2] -= rhs.e[2];
-    }
-}
-
-// vec乘double
-impl Mul<f64> for Vec3 {
-    type Output = Self;
-    fn mul(self, t: f64) -> Self {
-        Self::new(self.e[0] * t, self.e[1] * t, self.e[2] * t)
-    }
-}
-
-// double乘vec
-impl Mul<Vec3> for f64 {
-    type Output = Vec3;
-    fn mul(self, v: Vec3) -> Vec3 {
-        Vec3::new(self * v.e[0], self * v.e[1], self * v.e[2])
-    }
-}
-
-// vec乘vec
-impl Mul<Vec3> for Vec3 {
-    type Output = Vec3;
-    fn mul(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] * rhs.e[0],
-            self.e[1] * rhs.e[1],
-            self.e[2] * rhs.e[2],
-        )
-    }
-}
-
-// vec乘double 乘等于
-impl MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, t: f64) {
-        self.e[0] *= t;
-        self.e[1] *= t;
-        self.e[2] *= t;
-    }
-}
-
-// vec除以double
-impl Div<f64> for Vec3 {
-    type Output = Self;
-    fn div(self, t: f64) -> Self {
-        self * (1.0 / t)
-    }
-}
-
-// vec除以double 除等于
-impl DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, t: f64) {
-        *self *= 1.0 / t;
-    }
-}
-
-// 实现输出格式化
-impl fmt::Display for Vec3 {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
-    }
-}
-
-impl Index<usize> for Vec3 {
-    type Output = f64;
-    fn index(&self, index: usize) -> &f64 {
-        &self.e[index]
-    }
-}
-
-impl IndexMut<usize> for Vec3 {
-    fn index_mut(&mut self, index: usize) -> &mut f64 {
-        &mut self.e[index]
-    }
-}
-
-// 向量点积
-pub fn dot(u: &Vec3, v: &Vec3) -> f64 {
-    u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]
-}
-
-// 向量叉乘
-pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
-    Vec3::new(
-        u.e[1] * v.e[2] - u.e[2] * v.e[1],
-        u.e[2] * v.e[0] - u.e[0] * v.e[2],
-        u.e[0] * v.e[1] - u.e[1] * v.e[0],
-    )
-}
-
-// 单位向量
-pub fn unit_vector(v: Vec3) -> Vec3 {
-    v / v.length()
-}
-
-pub fn random_in_unit_disk() -> Vec3 {
-    loop {
-        let p = Vec3::new(
-            random_double_range(-1.0, 1.0),
-            random_double_range(-1.0, 1.0),
-            0.0,
-        );
-        if p.length_squared() < 1.0 {
-            return p;
+    pub fn from_rgb(red: f64, green: f64, blue: f64) -> Self {
+        Self {
+            albedo: Color::new(red, green, blue),
         }
     }
 }
 
-pub fn random_unit_vector() -> Vec3 {
-    loop {
-        let p = Vec3::random_range(-1.0, 1.0);
-        let lensq = p.length_squared();
-        if 1e-160 < lensq && lensq <= 1.0 {
-            return p / lensq.sqrt();
+impl Texture for SolidColor {
+    fn value(&self, _u: f64, _v: f64, _p: &Point3) -> Color {
+        self.albedo
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CheckerTexture {
+    inv_scale: f64,         // 缩放因子的倒数
+    even: Arc<dyn Texture>, // 偶数格子的纹理
+    odd: Arc<dyn Texture>,  // 奇数格子的纹理
+}
+
+impl CheckerTexture {
+    pub fn new(scale: f64, even: Arc<dyn Texture>, odd: Arc<dyn Texture>) -> Self {
+        Self {
+            inv_scale: 1.0 / scale,
+            even,
+            odd,
+        }
+    }
+
+    pub fn from_colors(scale: f64, c1: Color, c2: Color) -> Self {
+        Self {
+            inv_scale: 1.0 / scale,
+            even: Arc::new(SolidColor::new(c1)),
+            odd: Arc::new(SolidColor::new(c2)),
         }
     }
 }
 
-pub fn random_on_hemisphere(normal: &Vec3) -> Vec3 {
-    let on_unit_sphere = random_unit_vector();
-    if dot(&on_unit_sphere, normal) > 0.0 {
-        return on_unit_sphere;
+impl Texture for CheckerTexture {
+    fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
+        let x_integer = (self.inv_scale * p.x()).floor() as i32;
+        let y_integer = (self.inv_scale * p.y()).floor() as i32;
+        let z_integer = (self.inv_scale * p.z()).floor() as i32;
+
+        let is_even = (x_integer + y_integer + z_integer) % 2 == 0;
+
+        if is_even {
+            self.even.value(u, v, p)
+        } else {
+            self.odd.value(u, v, p)
+        }
     }
-    -on_unit_sphere
 }
 
-pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
-    *v - 2.0 * dot(v, n) * *n
+#[derive(Debug)]
+pub struct ImageTexture {
+    rtw_image: RtwImage,
 }
 
-pub fn refract(uv: &Vec3, n: &Vec3, etai_over_etat: f64) -> Vec3 {
-    let cos_theta = (-dot(uv, n)).min(1.0);
-    let r_out_perp = etai_over_etat * (*uv + cos_theta * *n);
-    let discriminant = 1.0 - r_out_perp.length_squared();
-    let r_out_parallel = -discriminant.sqrt() * *n;
-    r_out_perp + r_out_parallel
+impl ImageTexture {
+    pub fn new(filename: &str) -> Self {
+        Self {
+            rtw_image: RtwImage::new(filename),
+        }
+    }
+}
+
+impl Texture for ImageTexture {
+    fn value(&self, u: f64, v: f64, _p: &Point3) -> Color {
+        if self.rtw_image.height() == 0 {
+            return Color::new(0.0, 1.0, 1.0);
+        }
+
+        let u = u.clamp(0.0, 1.0);
+        let v = 1.0 - v.clamp(0.0, 1.0);
+
+        let i = (u * self.rtw_image.width() as f64) as u32;
+        let j = (v * self.rtw_image.height() as f64) as u32;
+
+        let pixel = self.rtw_image.pixel_data(i, j);
+
+        let color_scale = 1.0 / 255.0;
+        Color::new(
+            color_scale * pixel[0] as f64,
+            color_scale * pixel[1] as f64,
+            color_scale * pixel[2] as f64,
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct NoiseTexture {
+    noise: Perlin,
+    scale: f64,
+}
+
+impl NoiseTexture {
+    pub fn new(scale: f64) -> Self {
+        Self {
+            noise: Perlin::new(),
+            scale,
+        }
+    }
+}
+
+impl Texture for NoiseTexture {
+    fn value(&self, _u: f64, _v: f64, p: &Point3) -> Color {
+        let noise_value = self.noise.turb(&*p, 7);
+        Color::new(0.5, 0.5, 0.5) * (1.0 + (self.scale * p.z() + 10.0 * noise_value).sin())
+    }
 }
